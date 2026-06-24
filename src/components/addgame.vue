6d8152400a39d143f9580a9ddc516e399374f20f
mayzeess <ananevviktor22@gmail.com>
2026-06-24 13:32:01 +0400

    Исправил название компонентов. Добавлены глобальные стили для aboutInfo и homeInfo. выполнил issueid #1.

--- src/components/addgame.vue
+++ src/components/addgame.vue
@@ -1,17 +1,15 @@
 <template>
     <div class="main-div">
         <p class="text-white text-left text-2xl mt-8 mb-5" v-if="(info != '')">{{ info }}</p>
-        <form class="w-full max-w-lg flex flex-col gap-4 text-white px-4 text-lg">
+        <form class="w-full max-w-lg flex flex-col gap-4 text-white px-4 text-lg" @submit.prevent="addGame">
             <img :src="imageGame" class="max-w-full max-h-[20vh] lg:max-h-[25vh] 2xl:max-h-[35vh] block rounded-lg mx-auto" v-if="imageGame != ''"/>
             <input class="cursor-pointer input" type="file" accept="image/*" @change="handleImage">
             <input class="input" type="text" placeholder="Название" v-model="nameGame">
             <input class="accent-purple-600 cursor-pointer" type="range" min="1" max="10" step="1" placeholder="Рейтинг" v-model.number="ratingGame">
             <p class="text-xl mx-auto font-bold">Оценка игры: {{ ratingGame }}/10</p>
             <textarea class="resize-y input" placeholder="Комментарий" v-model="commentGame"></textarea>
+            <button class="button m-3 mx-auto">Добавить</button>
         </form>
-        <button class="button m-3" @click="addGame()">
-        Добавить
-    </button>
     </div>
 </template>
 
@@ -41,7 +39,9 @@ const addGame = () => {
         info.value = 'Введите название игры'
         return
     }
-    
+    if (commentGame.value == ''){
+        commentGame.value = 'Без комментариев'
+    }
     store.addGame({
         id: Date.now(),
         image: imageGame.value,
--- src/components/editgame.vue
+++ src/components/editgame.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="main-div  pt-10">
     <p class="text-white text-center text-left text-2xl" v-if="(info != '')">{{ info }}</p>
-    <form class="w-full max-w-lg flex flex-col gap-4 mt-8 text-white px-4 text-lg">
+    <form class="w-full max-w-lg flex flex-col gap-4 mt-8 text-white px-4 text-lg" @submit.prevent="GameEdit">
         <img :src="imageGame" class="max-w-full max-h-[20vh] lg:max-h-[25vh] 2xl:max-h-[35vh] block rounded-lg mx-auto" v-if="imageGame != ''"/>
         <input class="cursor-pointer input" type="file" accept="image/*" @change="handleImage">
         <input class="input" type="text" placeholder="Название" v-model="nameGame">
         <input class="accent-purple-600 cursor-pointer" type="range" min="1" max="10" step="1" placeholder="Рейтинг" v-model.number="ratingGame">
         <p class="text-xl mx-auto font-bold">Оценка игры: {{ ratingGame }}/10</p>
         <textarea class="resize-y input" placeholder="Комментарий" v-model="commentGame"></textarea>
+        <button class="button m-3 mx-auto">Подтвердить</button>
     </form>
-        <button class="button m-3" @click="GameEdit">Подтвердить</button>
     </div>
 </template>
 
@@ -47,6 +47,9 @@ const GameEdit = () => {
     } else if (nameGame.value === ''){
         info.value = 'Введите название игры'
         return
+    }
+    if (commentGame.value == ''){
+        commentGame.value = 'Без комментариев'
     }
    
     store.editGame({
