32cfdd5a32a4393ea3fa1493e3632588941c5625
mayzeess <ananevviktor22@gmail.com>
2026-06-24 18:38:18 +0400

    подключение express сервера к prisma, возвращение данных через sqlite

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import { prisma } from './prisma'
 
 const app = express()
 const PORT = 3000
@@ -11,8 +12,13 @@ app.get('/', (_req, res) => {
     res.send('работает')
 })
 
-app.get('/api/games', (_req, res) => {
-    res.json([])
+app.get('/api/games', async (_req, res) => {
+    const games = await prisma.game.findMany({
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+    res.json(games)
 })
 
 app.listen(PORT, () => {
