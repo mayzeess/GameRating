1332c01892de7dc2ce175cae5afeeb025a820fa6
mayzeess <ananevviktor22@gmail.com>
2026-06-24 19:05:00 +0400

    backend: создан post запрос

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -24,3 +24,29 @@ app.get('/api/games', async (_req, res) => {
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`)
 })
+
+app.post('/api/games', async (req, res) => {
+    const { name, rating, comment, image } = req.body
+
+    const ratingNumber = Number(rating)
+    if (!name || String(name).trim() === '') {
+        res.status(400).json({ message: 'нет названия игры' })
+        return
+    }
+
+    if (!image || String(image).trim() === '') {
+        res.status(400).json({ message: 'нету изображения' })
+        return
+    }
+
+    const game = await prisma.game.create({
+        data: {
+            name: String(name).trim(),
+            rating: ratingNumber,
+            comment: String(comment || '').trim() || 'Без комментариев',
+            image: String(image).trim()
+        }
+    })
+
+    res.status(201).json(game)
+})
